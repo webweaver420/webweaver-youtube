42581bed2729c915bbcfa2216bbb12d250fab5e4
an83690880 <166381564+webweaver420@users.noreply.github.com>
2024-05-20 17:03:33 +0900

    1111

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route pate='/' />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
